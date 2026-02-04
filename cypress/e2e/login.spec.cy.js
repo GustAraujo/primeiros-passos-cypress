3aa96c0c8622b8096f9ba2c18f0a1398a29c8fda
Gustavo Araujo <Gustavo_araujoL@hotmail.com>
2026-02-04 14:06:11 -0300

    Primeiro commit

---
+++ cypress/e2e/login.spec.cy.js
@@ -0,0 +1,18 @@
+describe('OrangeHRM Test', () => {
+  it('Login success', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get('[name="username"]').type('Admin')
+    cy.get('[name="password"]').type('admin123')
+    cy.get('[type="submit"]').click()
+    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
+    cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').contains('Dashboard')
+  })
+
+  it('Login failed', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get('[name="username"]').type('test')
+    cy.get('[name="password"]').type('wrongpassword')
+    cy.get('[type="submit"]').click()
+    cy.get('.oxd-alert').contains('Invalid credentials')
+  })
+})
