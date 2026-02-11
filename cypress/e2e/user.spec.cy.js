4f97ff30c9abf93538f256505f3ce665133c337d
Gustavo Araujo <Gustavo_araujoL@hotmail.com>
2026-02-11 15:23:57 -0300

    adicionando page objects

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -1,44 +1,54 @@
 import userData from '../fixtures/user-data.json'
+import LoginPage from '../../pages/loginPage.js'
+import dashboardPage from '../../pages/dashboardPage.js'
+import menuPage from '../../pages/menuPage.js'
+
+const loginPage = new LoginPage()
+
+
 
 describe('OrangeHRM Test', () => {
 
   const selectorsList = {
-    usernameField: '[name="username"]',
-    passwordField: '[name="password"]',
-    submitButton: '[type="submit"]',
-    titleFieldTopBar: '.oxd-topbar-header-breadcrumb > .oxd-text',
-    dashboardGrid: '.orangehrm-dashboard-grid',
-    wrongCreditialsAlert: '.oxd-alert',
-    myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]',
+    
     firstNameField: '[name="firstName"]',
     lastNameField: '[name="lastName"]',
     genericIDField: '.oxd-input--active',
     dateField: '[placeholder="yyyy-dd-mm"]',
     dateCloseButton: '.--close',
     saveButton: '[type="submit"]',
+    listButton: '.oxd-select-text--arrow',
+
+
   } 
 
   it.only('User Info Update - success', () => {
-    cy.visit('/auth/login')
-    cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
-    cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
-    cy.get(selectorsList.submitButton).click()
-    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get(selectorsList.dashboardGrid)
-    cy.get(selectorsList.myInfoButton).click()
-    cy.get(selectorsList.firstNameField).clear().type('FirstNameTest')
-    cy.get(selectorsList.lastNameField).clear().type('LastNameTest')
-    cy.get(selectorsList.genericIDField).eq(3).clear().type('employee')
-    cy.get(selectorsList.genericIDField).eq(4).clear().type('IDNameTest')
-    cy.get(selectorsList.genericIDField).eq(5).clear().type('DriverNameTest')
-    cy.get(selectorsList.genericIDField).eq(8).clear().type('TestNameTest')
-    cy.get(selectorsList.dateField).eq(0).clear().type('2030-07-22')
-    cy.get(selectorsList.dateCloseButton).click()
-    cy.get(selectorsList.dateField).eq(1).clear().type('2000-04-02')
-    cy.get(selectorsList.dateCloseButton).click()
-    cy.get(selectorsList.saveButton).eq(0).click()
-    cy.get('body').should('contain', 'Successfully Updated')
-    cy.get('.oxd-toast-close')
+    
+    loginPage.accessLoginPage()
+    loginPage.loginWithUser(userData.userSuccess.username, userData.userSuccess.password)
+
+    dashboardPage.checkDashboardPage()
+    menuPage.accessMyInfo()
+    
+    
+    //cy.get(selectorsList.myInfoButton).click()
+    // cy.get(selectorsList.firstNameField).clear().type('FirstNameTest')
+    // cy.get(selectorsList.lastNameField).clear().type('LastNameTest')
+    // cy.get(selectorsList.genericIDField).eq(3).clear().type('employee')
+    // cy.get(selectorsList.genericIDField).eq(4).clear().type('IDNameTest')
+    // cy.get(selectorsList.genericIDField).eq(5).clear().type('DriverNameTest')
+    // cy.get(selectorsList.genericIDField).eq(8).clear().type('TestNameTest')
+    // cy.get(selectorsList.dateField).eq(0).clear().type('2030-07-22')
+    // cy.get(selectorsList.dateCloseButton).click()
+    // cy.get(selectorsList.dateField).eq(1).clear().type('2000-04-02')
+    // cy.get(selectorsList.dateCloseButton).click()
+    // cy.get(selectorsList.listButton).eq(0).click()
+    // cy.get('.oxd-select-dropdown').contains('Brazilian').click()
+    // cy.get(selectorsList.listButton).eq(1).click()
+    // cy.get('.oxd-select-dropdown').contains('Single').click()
+    // cy.get(selectorsList.saveButton).eq(0).click()
+    // cy.get('body').should('contain', 'Successfully Updated')
+    // cy.get('.oxd-toast-close')
     })
 
   it('Login failed', () => {
---
+++ pages/loginPage.js
@@ -0,0 +1,25 @@
+class LoginPage {
+    selectorsList() {
+        const selectors = {
+            usernameField: '[name="username"]',
+            passwordField: '[name="password"]',
+            submitButton: '[type="submit"]',
+            wrongCreditialsAlert: '.oxd-alert',
+        }
+
+        return selectors
+    }
+
+    accessLoginPage() {
+        cy.visit('/auth/login')
+    }
+
+    loginWithUser(username, password) {
+        cy.get(this.selectorsList().usernameField).type(username)
+        cy.get(this.selectorsList().passwordField).type(password)
+        cy.get(this.selectorsList().submitButton).click()
+    }
+
+}
+
+export default LoginPage
